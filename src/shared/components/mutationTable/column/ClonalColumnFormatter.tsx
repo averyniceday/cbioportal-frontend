727fee132299c4c96fe57b4224558479e97e50b2
Avery Wang <wanga5@mskcc.org>
2018-12-13 09:52:26 -0500

    adding circle icons for Clonal

--- src/shared/components/mutationTable/column/ClonalColumnFormatter.tsx
+++ src/shared/components/mutationTable/column/ClonalColumnFormatter.tsx
@@ -24,26 +24,80 @@ export default class ClonalColumnFormatter {
      * @returns {string}"Clonal" text value
      */
     public static getDisplayValue(data:Mutation[], sampleIds:string[]) {
-        let values:string[] = [];
-        const sampleToValue:{[key: string]: any} = {};
+        let values = [];
+        const sampleToValue:{[key: string]: string} = {};
+        const sampleToCCFMCopiesUpper:{[key: string]: number} = {};
+        
         for (const mutation of data) {
             sampleToValue[mutation.sampleId] = ClonalColumnFormatter.getClonalValue([mutation]);
+        }
+        for (const mutation of data) {
+            sampleToCCFMCopiesUpper[mutation.sampleId] = mutation.ccfMCopiesUpper;
         }
         // exclude samples with invalid count value (undefined || emtpy || lte 0)
         const samplesWithValue = sampleIds.filter(sampleId =>
             sampleToValue[sampleId] && sampleToValue[sampleId].toString().length > 0);
 
         // single value: just add the actual value only
-        if (samplesWithValue.length === 1) {
-            values = [sampleToValue[samplesWithValue[0]].toString()];
+        let tdValue = null;
+        if (!samplesWithValue) {
+            return (<span></span>);
+        } else if (samplesWithValue.length === 1) {
+             tdValue = <li><DefaultTooltip overlay={ClonalColumnFormatter.getTooltip(`${samplesWithValue[0]}`, `${sampleToValue[samplesWithValue[0]]}`, `${sampleToCCFMCopiesUpper[samplesWithValue[0]]}`)} placement="left" arrowContent={<div className="rc-tooltip-arrow-inner"/>}>{ClonalColumnFormatter.getClonalCircle(sampleToValue[samplesWithValue[0]])}</DefaultTooltip></li>;
         }
         // multiple value: add sample id and value pairs
         else {
-            values = samplesWithValue.map((sampleId:string) => (`${sampleId}: ${sampleToValue[sampleId]}`));
+             tdValue = samplesWithValue.map((sampleId:string) => {
+                return (
+                    <li><DefaultTooltip overlay={ClonalColumnFormatter.getTooltip(`${sampleId}`, `${sampleToValue[sampleId]}`, `${sampleToCCFMCopiesUpper[sampleId]}`)} placement="left" arrowContent={<div className="rc-tooltip-arrow-inner"/>}>{ClonalColumnFormatter.getClonalCircle(`${sampleToValue[sampleId]}`)}</DefaultTooltip></li>
+                );
+            });
         }
-        return values.join("\n");
+        return (
+                <span style={{display:'inline-block', minWidth:100}}>
+                    <ul style={{marginBottom:0}} className="list-inline list-unstyled">{ tdValue }</ul>
+                </span> 
+               );
     }
-       
+    
+    public static getTooltip(sampleId:string, clonalValue:string, ccfMCopiesUpperValue:string) {
+        let clonalDisplay = "";
+        if (clonalValue === "yes") {
+            clonalDisplay = "Y";
+        } else if  (clonalValue === "no") {
+            clonalDisplay = "N";
+        } else {
+            clonalDisplay = "NA";
+        }
+        if (clonalDisplay === "NA") {
+            return (<span></span>);
+        } 
+        return (<span>
+                    <strong>{sampleId} ({clonalDisplay})</strong>: {ccfMCopiesUpperValue}
+                </span>
+        );  
+    }
+
+    public static getClonalCircle(clonalValue:string) {
+        let color:string = "";
+        let letter:string = "";
+        if (clonalValue === "yes") {
+            color = "limegreen";
+            letter = "Y";
+        } else if (clonalValue === "no") {
+            color = "dimgrey";
+            letter = "N";
+        } else {
+            color = "lightgrey";
+            letter = "NA";
+        }
+        return (
+                <svg height="10" width="10">
+                    <circle cx={5} cy={5} r={5} fill={`${color}`}/>
+                </svg>
+        );
+    }
+ 
     public static getCcfMCopiesUpperValue(data:Mutation[]):number {
         const ccfMCopiesUpperValue = data[0].ccfMCopiesUpper;
         return ccfMCopiesUpperValue;
@@ -69,9 +123,19 @@ export default class ClonalColumnFormatter {
 
     public static renderFunction(data:Mutation[], sampleIds:string[]) {
         // use text for all purposes (display, sort, filter)
+        return ClonalColumnFormatter.getDisplayValue(data,sampleIds);
+        /*
         const text:string = ClonalColumnFormatter.getDisplayValue(data, sampleIds);
-        let content = <span>{text}</span>;
-        return content;
+        if (text === "yes") {
+            return (<svg>
+                        <circle cx={10} cy={10} r={5} fill="green" />
+                    </svg>
+            );
+        } else {
+            let content = <span>{text}</span>;
+            return content;
+        }
+        */
     }
     
     // can be removed if mouseover is dropped
